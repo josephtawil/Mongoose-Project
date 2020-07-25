f8f5700eb2389442365480c4ea9b27d4f3984c17
Joseph <jtawil2015@gmail.com>
2020-07-25 10:44:58 -0800

    created our schema

--- models/shoe.js
+++ models/shoe.js
@@ -1,6 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const shoeSchema = new Schema([]);
 
+// defining our Schema created our entities and how our data is put together
+const shoeSchema = new Schema({
+    brand: { 
+        type: String,
+        //unique: true
+        required: "Must enter a shoe brand"
+    },
+
+    price: { 
+        type: Number,
+        required: "Must enter a price",
+    },
+    
+    model: {
+        type: String,
+        unique: "Cant have more of the same model"
+    },
+    
+    colorWay: {
+        type: [String],
+        required: true,
+        default: [],
+    },
+});
+
+//we are naming our schema as Shoe
 const Shoe = mongoose.model("Shoe", shoeSchema);
+
+module.exports = Shoe;
