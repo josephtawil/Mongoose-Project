2e089cba08ac264ac2eb9c69b05538a5c6b3737c
Joseph <jtawil2015@gmail.com>
2020-07-25 11:18:15 -0800

    added new get route

--- routes/shoe-routes.js
+++ routes/shoe-routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {addShoe} = require('../controllers/shoe-controller');
+const {addShoe, getShoe} = require('../controllers/shoe-controller');
 
+//this will create a shoe
 router.post("/addShoe", addShoe);
-
+//this will get all the Shoes
+router.get('/allShoes', getShoe);
 module.exports = router;
